b34e65dde3a23ecf100942e41c23dfe02e69b0a0
gaoyong <gaoyong_chn@163.com>
2023-11-04 17:20:05 +0800

    uwb

--- real_shfiles/uwb.sh
+++ real_shfiles/uwb.sh
@@ -3,5 +3,5 @@ ag_n=$1
 sudo chmod 777 /dev/ttyACM1 & sleep 2;
 source devel/setup.bash;
 
-roslaunch msg_utils uwb_transfer.launch ag_n:=${ag_n};
+roslaunch msg_utils uwb_transfer.launch ag_n:=${ag_n} & sleep 2;
 roslaunch nlink_parser linktrack.launch  & sleep 2;
